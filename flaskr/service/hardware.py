fd7c309997a2ccf666b147fe4213b59e915e756d
fiore93 <florianoros93@gmail.com>
2024-01-29 22:56:38 +0100

    add nl
    add nl

--- flaskr/service/hardware.py
+++ flaskr/service/hardware.py
@@ -1,8 +1,6 @@
-import os
-
 class Hardware:
+
     def get_cpu_temp(self):
         with open("/sys/class/thermal/thermal_zone0/temp", "r") as file:
             temp = float(file.read()) / 1000
         return temp
-
